ba6a7b10cbf575a419b03c62436c954bbf049d87
Natacha-A <90852900+Natacha-A@users.noreply.github.com>
2022-02-20 19:11:33 -0500

    tp02 zone contenu principal 1 à 3
    Bouton en savoir plus ne marche pas. Est-ce que j'ai besoin du js pour le faire scroller vers la section du contenu principa?

---
+++ scripts/home.js
@@ -0,0 +1,7 @@
+/*const main = document.querySelector("#main");
+
+const btnSavoirPlus = document.getElementById("btnSavoirPlus");
+
+btnSavoirPlus.addEventListener('click', function () {
+    window.scrollTo({ left: 0, top: main, behavior: 'smooth' });
+  });*/
