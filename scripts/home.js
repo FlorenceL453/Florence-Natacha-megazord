0404cf4742fb76efb8a5143a6466fc042e675897
Natacha-A <90852900+Natacha-A@users.noreply.github.com>
2022-03-13 18:09:00 -0400

    correction Hauteur Img Groupe

--- scripts/home.js
+++
@@ -1,7 +0,0 @@
-/*const main = document.querySelector("#main");
-
-const btnSavoirPlus = document.getElementById("btnSavoirPlus");
-
-btnSavoirPlus.addEventListener('click', function () {
-    window.scrollTo({ left: 0, top: main, behavior: 'smooth' });
-  });*/
