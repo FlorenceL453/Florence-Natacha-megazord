7cc29c78dc520f9a633df4607c3434dd4eea1f91
Natacha-A <90852900+Natacha-A@users.noreply.github.com>
2022-03-29 16:40:27 -0400

    tp05 fini
    animation des cartes, du carrousel et de la vidéo.

--- scripts/home.js
+++ scripts/home.js
@@ -7,12 +7,17 @@ const sectionsArr = document.querySelectorAll('.contenuP section');
         scrollTrigger: {
             markers: true,
             start: 'top 75%',
-            end: 'bottom 25%',
+            end: 'bottom 75%',
             toggleActions: 'restart complete reverse reset',
             trigger: sectionP,
         },
     })
     //Animer tous les titres2
-    .fromTo('.title-2', {x:'100%', opacity:0, scale:2, rotation:180, skew:360}, 
-        {x:'0%', opacity:1, scale:1, rotation:0, ease:'back', skew:0})
+    .fromTo('.title-2', {x:'100%', opacity:0, scale:2, rotation:180, skew:360}, {x:'0%', opacity:1, scale:1, rotation:0, ease:'back', skew:0})
+    //Animer les cartes
+    .fromTo('.carteMembre', {opacity:0, scale:2, rotation:360, skew:180}, {opacity:1, scale:1, rotation:0, skew:0, ease:'back'}, '<')
+    //Animer le carrousel
+    .fromTo('.Carrousel', {opacity:0, scale:2, rotation:360, skew:180}, {opacity:1, scale:1, rotation:0, skew:0, ease:'back'})
+    //Animer la vidéo
+    .fromTo('.video', {opacity:0, scale:2, rotation:360, skew:180}, {opacity:1, scale:1, rotation:0, skew:0, ease:'back'})
  });
