6d58e9459e3c685ca147ba2451d0c3f085315c4c
Natacha-A <90852900+Natacha-A@users.noreply.github.com>
2022-03-29 17:09:29 -0400

    joue apres anim des titres

--- scripts/home.js
+++ scripts/home.js
@@ -15,7 +15,7 @@ const sectionsArr = document.querySelectorAll('.contenuP section');
     //Animer tous les titres2
     .fromTo('.title-2', {x:'100%', opacity:0, scale:2, rotation:180, skew:360}, {x:'0%', opacity:1, scale:1, rotation:0, ease:'back', skew:0})
     //Animer les cartes
-    .fromTo('.carteMembre', {opacity:0, scale:2, rotation:360, skew:180}, {opacity:1, scale:1, rotation:0, skew:0, ease:'back'}, '<')
+    .fromTo('.carteMembre', {opacity:0, scale:2, rotation:360, skew:180}, {opacity:1, scale:1, rotation:0, skew:0, ease:'back'})
     //Animer le carrousel
     .fromTo('.Carrousel', {opacity:0, scale:2, rotation:360, skew:180}, {opacity:1, scale:1, rotation:0, skew:0, ease:'back'})
     //Animer la vidéo
