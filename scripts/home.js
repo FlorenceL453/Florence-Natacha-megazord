04fe0d525823da73c7bccfb81036f6261c4ec1ec
Natacha-A <90852900+Natacha-A@users.noreply.github.com>
2022-03-30 15:43:36 -0400

    position absolute

--- scripts/home.js
+++ scripts/home.js
@@ -6,18 +6,18 @@ const sectionsArr = document.querySelectorAll('.contenuP section');
         duration:2,
         scrollTrigger: {
             markers: true,
-            start: 'top 75%',
-            end: 'bottom 75%',
+            start: 'top 50%',
+            end: 'center 75%',
             toggleActions: 'restart complete reverse reset',
             trigger: sectionP,
         },
     })
     //Animer tous les titres2
-    .fromTo('.title-2', {x:'100%', opacity:0, scale:2, rotation:180, skew:360}, {x:'0%', opacity:1, scale:1, rotation:0, ease:'back', skew:0})
+    .fromTo('.title-2', {x:'100%', opacity:0, scale:2, rotation:180, skew:360}, {x:'0%', opacity:1, scale:1, rotation:0, ease:'back', skew:0}, '1')
     //Animer les cartes
     .fromTo('.carteMembre', {opacity:0, scale:2, rotation:360, skew:180}, {opacity:1, scale:1, rotation:0, skew:0, ease:'back'}, '<')
     //Animer le carrousel
-    .fromTo('.Carrousel', {opacity:0, scale:2, rotation:360, skew:180}, {opacity:1, scale:1, rotation:0, skew:0, ease:'back'}, '-=0.25')
+    .fromTo('.Carrousel', {opacity:0, scale:2, rotation:360, skew:180}, {opacity:1, scale:1, rotation:0, skew:0, ease:'back'}, '2')
     //Animer la vidéo
-    .fromTo('.video', {opacity:0, scale:2, rotation:360, skew:180}, {opacity:1, scale:1, rotation:0, skew:0, ease:'back'})
+    .fromTo('.video', {opacity:0, scale:2, rotation:360, skew:180}, {opacity:1, scale:1, rotation:0, skew:0, ease:'back'}, '3')
  });
