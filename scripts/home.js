3b5df01042ca4ea05653f44a1eb86a41a304f97b
Natacha-A <90852900+Natacha-A@users.noreply.github.com>
2022-03-30 15:34:08 -0400

    animation temps

--- scripts/home.js
+++ scripts/home.js
@@ -3,9 +3,9 @@ gsap.registerPlugin(ScrollTrigger);
 const sectionsArr = document.querySelectorAll('.contenuP section');
  sectionsArr.forEach(function(sectionP){
     gsap.timeline({
-        duration:5,
+        duration:2,
         scrollTrigger: {
-            markers: false,
+            markers: true,
             start: 'top 75%',
             end: 'bottom 75%',
             toggleActions: 'restart complete reverse reset',
@@ -15,9 +15,9 @@ const sectionsArr = document.querySelectorAll('.contenuP section');
     //Animer tous les titres2
     .fromTo('.title-2', {x:'100%', opacity:0, scale:2, rotation:180, skew:360}, {x:'0%', opacity:1, scale:1, rotation:0, ease:'back', skew:0})
     //Animer les cartes
-    .fromTo('.carteMembre', {opacity:0, scale:2, rotation:360, skew:180}, {opacity:1, scale:1, rotation:0, skew:0, ease:'back'})
+    .fromTo('.carteMembre', {opacity:0, scale:2, rotation:360, skew:180}, {opacity:1, scale:1, rotation:0, skew:0, ease:'back'}, '<')
     //Animer le carrousel
-    .fromTo('.Carrousel', {opacity:0, scale:2, rotation:360, skew:180}, {opacity:1, scale:1, rotation:0, skew:0, ease:'back'})
+    .fromTo('.Carrousel', {opacity:0, scale:2, rotation:360, skew:180}, {opacity:1, scale:1, rotation:0, skew:0, ease:'back'}, '-=0.25')
     //Animer la vidéo
     .fromTo('.video', {opacity:0, scale:2, rotation:360, skew:180}, {opacity:1, scale:1, rotation:0, skew:0, ease:'back'})
  });
