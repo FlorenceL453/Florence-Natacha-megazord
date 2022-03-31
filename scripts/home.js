84a12fccb003090107cb6347715487211ad2bbf7
Natacha-A <90852900+Natacha-A@users.noreply.github.com>
2022-03-31 09:56:48 -0400

    ajuster animation probleme boucle

--- scripts/home.js
+++ scripts/home.js
@@ -13,11 +13,15 @@ const sectionsArr = document.querySelectorAll('.contenuP section');
         },
     })
     //Animer tous les titres2
-    .fromTo('.title-2', {x:'100%', opacity:0, scale:2, rotation:180, skew:360}, {x:'0%', opacity:1, scale:1, rotation:0, ease:'back', skew:0}, '1')
+    .fromTo('.SectionMembre .title-2', {x:'100%', opacity:0, scale:2, rotation:180, skew:360}, {x:'0%', opacity:1, scale:1, rotation:0, ease:'back', skew:0})
     //Animer les cartes
     .fromTo('.carteMembre', {opacity:0, scale:2, rotation:360, skew:180}, {opacity:1, scale:1, rotation:0, skew:0, ease:'back'}, '<')
+    //Animer le titre du carrousel
+    .fromTo('.Carrousel .title-2', {x:'100%', opacity:0, scale:2, rotation:180, skew:360}, {x:'0%', opacity:1, scale:1, rotation:0, ease:'back', skew:0}, '+=0.25')
     //Animer le carrousel
-    .fromTo('.Carrousel', {opacity:0, scale:2, rotation:360, skew:180}, {opacity:1, scale:1, rotation:0, skew:0, ease:'back'}, '2')
+    .fromTo('.Carrousel', {opacity:0, scale:2, rotation:360, skew:180}, {opacity:1, scale:1, rotation:0, skew:0, ease:'back'}, '<')
+    //Animer le titre de la section vidéo
+    .fromTo('.video .title-2', {x:'100%', opacity:0, scale:2, rotation:180, skew:360}, {x:'0%', opacity:1, scale:1, rotation:0, ease:'back', skew:0}, '+=0.50')
     //Animer la vidéo
-    .fromTo('.video', {opacity:0, scale:2, rotation:360, skew:180}, {opacity:1, scale:1, rotation:0, skew:0, ease:'back'}, '3')
+    .fromTo('.video', {opacity:0, scale:2, rotation:360, skew:180}, {opacity:1, scale:1, rotation:0, skew:0, ease:'back'}, '<')
  });
