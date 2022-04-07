a0c3e4e74be419fe1bed8023f61b301aef15e070
Natacha-A <90852900+Natacha-A@users.noreply.github.com>
2022-04-07 11:58:58 -0400

    correction animations wip
    envoyer message au prof pour l'animation et pour les images du slider.

--- scripts/home.js
+++ scripts/home.js
@@ -5,23 +5,23 @@ const sectionsArr = document.querySelectorAll('.contenuP section');
     gsap.timeline({
         duration:2,
         scrollTrigger: {
-            markers: false,
-            start: 'top 50%',
-            end: 'center 75%',
+            markers: true,
+            start: 'top 75%',
+            end: 'bottom 15%',
             toggleActions: 'restart complete reverse reset',
             trigger: sectionP,
         },
     })
     //Animer tous les titres2
-    .fromTo('.SectionMembre .title-2', {x:'100%', opacity:0, scale:2, rotation:180, skew:360}, {x:'0%', opacity:1, scale:1, rotation:0, ease:'back', skew:0})
+    .fromTo(sectionP.querySelector('.SectionMembre .title-2'), {x:'100%', opacity:0, rotation:180, skew:360}, {x:'0%', opacity:1, rotation:0, ease:'back', skew:0})
     //Animer les cartes
-    .fromTo('.carteMembre', {opacity:0, scale:2, rotation:360, skew:180}, {opacity:1, scale:1, rotation:0, skew:0, ease:'back'}, '<')
+    .fromTo('.carteMembre', {opacity:0, rotation:360, skew:180}, {opacity:1, rotation:0, skew:0, ease:'back'}, '<')
     //Animer le titre du carrousel
-    .fromTo('.Carrousel .title-2', {x:'100%', opacity:0, scale:2, rotation:180, skew:360}, {x:'0%', opacity:1, scale:1, rotation:0, ease:'back', skew:0}, '+=0.25')
+    .fromTo(sectionP.querySelector('.Carrousel .title-2'), {x:'100%', opacity:0, rotation:180, skew:360}, {x:'0%', opacity:1, rotation:0, ease:'back', skew:0}, '+=0.25')
     //Animer le carrousel
-    .fromTo('.Carrousel', {opacity:0, scale:2, rotation:360, skew:180}, {opacity:1, scale:1, rotation:0, skew:0, ease:'back'}, '<')
+    .fromTo('.Carrousel', {opacity:0, rotation:360, skew:180}, {opacity:1, rotation:0, skew:0, ease:'back'}, '<')
     //Animer le titre de la section vidéo
-    .fromTo('.video .title-2', {x:'100%', opacity:0, scale:2, rotation:180, skew:360}, {x:'0%', opacity:1, scale:1, rotation:0, ease:'back', skew:0}, '+=0.50')
+    .fromTo(sectionP.querySelector('.video .title-2'), {x:'100%', opacity:0, rotation:180, skew:360}, {x:'0%', opacity:1, rotation:0, ease:'back', skew:0}, '+=0.50')
     //Animer la vidéo
-    .fromTo('.video', {opacity:0, scale:2, rotation:360, skew:180}, {opacity:1, scale:1, rotation:0, skew:0, ease:'back'}, '<')
+    .fromTo('.video', {opacity:0, rotation:360, skew:180}, {opacity:1, rotation:0, skew:0, ease:'back'}, '<')
  });
