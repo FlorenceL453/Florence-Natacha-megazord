950dda343b74a52dea9da365abae88138dae212a
Natacha-A <90852900+Natacha-A@users.noreply.github.com>
2022-04-11 15:17:08 -0400

    correction animation gsap section
    scroll down tous marche, les sections font leur animation, une à la fois. Problème avec les img promotionnelles au scroll-up.

--- scripts/home.js
+++ scripts/home.js
@@ -13,15 +13,17 @@ const sectionsArr = document.querySelectorAll('.contenuP section');
         },
     })
     //Animer tous les titres2
-    .fromTo(sectionP.querySelector('.SectionMembre .title-2'), {x:'100%', opacity:0, rotation:180, skew:360}, {x:'0%', opacity:1, rotation:0, ease:'back', skew:0})
+    .fromTo(sectionP.querySelector('.titreCarteMembre .title-2'), {x:'100%', opacity:0, rotation:180, skew:360}, {x:'0%', opacity:1, rotation:0, ease:'back', skew:0})
     //Animer les cartes
-    .fromTo('.carteMembre', {opacity:0, rotation:360, skew:180}, {opacity:1, rotation:0, skew:0, ease:'back'}, '<')
+    .fromTo(sectionP.querySelectorAll('.carteMembre'), {opacity:0, rotation:360, skew:180}, {opacity:1, rotation:0, skew:0, ease:'back'}, '<')
+    
     //Animer le titre du carrousel
     .fromTo(sectionP.querySelector('.Carrousel .title-2'), {x:'100%', opacity:0, rotation:180, skew:360}, {x:'0%', opacity:1, rotation:0, ease:'back', skew:0}, '+=0.25')
     //Animer le carrousel
-    .fromTo('.Carrousel', {opacity:0, rotation:360, skew:180}, {opacity:1, rotation:0, skew:0, ease:'back'}, '<')
+    .fromTo(sectionP.querySelector('.Carrousel .swiper'), {opacity:0, rotation:360, skew:180}, {opacity:1, rotation:0, skew:0, ease:'back'}, '<')
+    
     //Animer le titre de la section vidéo
     .fromTo(sectionP.querySelector('.video .title-2'), {x:'100%', opacity:0, rotation:180, skew:360}, {x:'0%', opacity:1, rotation:0, ease:'back', skew:0}, '+=0.50')
     //Animer la vidéo
-    .fromTo('.video', {opacity:0, rotation:360, skew:180}, {opacity:1, rotation:0, skew:0, ease:'back'}, '<')
+    .fromTo(sectionP.querySelector('.video .divIframe'), {opacity:0, rotation:360, skew:180}, {opacity:1, rotation:0, skew:0, ease:'back'}, '<')
  });
