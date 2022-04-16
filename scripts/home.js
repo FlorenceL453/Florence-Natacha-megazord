6a481581d5441545509a5e27499c0ef7f75c1e7f
Natacha-A <90852900+Natacha-A@users.noreply.github.com>
2022-04-16 18:25:28 -0400

    animations sections page
    anim prend 1seconde et les markeurs permet de jouer en inverse pour toutes les sections.

--- scripts/home.js
+++ scripts/home.js
@@ -3,29 +3,29 @@ gsap.registerPlugin(ScrollTrigger);
 const sectionsArr = document.querySelectorAll('.contenuP section');
  sectionsArr.forEach(function(sectionP){
     gsap.timeline({
-        duration:2,
+        duration:1,
         scrollTrigger: {
             markers: false,
             start: 'top 75%',
-            end: 'bottom 15%',
+            end: '50% 75%',
             toggleActions: 'restart complete reverse reset',
             trigger: sectionP,
         },
     })
     //Animer tous les titres2
-    .fromTo(sectionP.querySelector('.titreCarteMembre .title-2'), {x:'100%', opacity:0, rotation:180}, {x:'0%', opacity:1, rotation:0, ease:'back'})
+    .fromTo(sectionP.querySelector('.titreCarteMembre .title-2'), {x:'-100%', opacity:0, rotation:180}, {x:'0%', opacity:1, rotation:0, ease:'back'})
     //Animer les cartes
-    .fromTo(sectionP.querySelectorAll('.carteMembre'), {opacity:0, rotation:360}, {opacity:1, rotation:0, ease:'back'}, '<')
+    .fromTo(sectionP.querySelectorAll('.carteMembre'), {x:'-100%', opacity:0, rotation:360}, {x:'0%', opacity:1, rotation:0, ease:'back'}, '<')
     
     //Animer le titre du carrousel
-    .fromTo(sectionP.querySelector('.Carrousel .title-2'), {x:'100%', opacity:0, rotation:180}, {x:'0%', opacity:1, rotation:0, ease:'back'}, '+=0.25')
+    .fromTo(sectionP.querySelector('.Carrousel .title-2'), {x:'-100%', opacity:0, rotation:180}, {x:'0%', opacity:1, rotation:0, ease:'back'}, '+=0.25')
     //Animer le carrousel
-    .fromTo(sectionP.querySelector('.Carrousel .swiper'), {opacity:0, rotation:360}, {opacity:1, rotation:0, ease:'back'}, '<')
+    .fromTo(sectionP.querySelector('.Carrousel .swiper'), {x:'-100%', opacity:0, rotation:360}, {x:'0%', opacity:1, rotation:0, ease:'back'}, '<')
     
     //Animer le titre de la section vidéo
-    .fromTo(sectionP.querySelector('.video .title-2'), {x:'100%', opacity:0, rotation:180}, {x:'0%', opacity:1, rotation:0, ease:'back'}, '+=0.50')
+    .fromTo(sectionP.querySelector('.video .title-2'), {x:'-100%', opacity:0, rotation:180}, {x:'0%', opacity:1, rotation:0, ease:'back'}, '+=0.50')
     //Animer la vidéo
-    .fromTo(sectionP.querySelector('.video .divIframe'), {opacity:0, rotation:360}, {opacity:1, rotation:0, ease:'back'}, '<')
+    .fromTo(sectionP.querySelector('.video .divIframe'), {x:'-100%', opacity:0, rotation:360}, {x:'0%', opacity:1, rotation:0, ease:'back'}, '<')
  });
 
  const body = document.querySelector('body');
